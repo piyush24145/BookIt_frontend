14fae5bf74f39740993d6278813c9a497b7f7d64
Piyush Kumar <24145@iiitu.ac.in>
2025-10-31 00:30:04 +0530

    updtaed

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
+
 const BASE = import.meta.env.VITE_API_URL;
+
 export const api = axios.create({
   baseURL: BASE,
   headers: {
@@ -7,7 +9,9 @@ export const api = axios.create({
   },
   withCredentials: false,
 });
-export const getExperiences = () => api.get("/experiences");
-export const getExperience = (id: string) => api.get(`/experiences/${id}`);
-export const createBooking = (payload: any) => api.post("/bookings", payload);
-export const validatePromo = (code: string) => api.post("/promo/validate", { code });
+
+export const getExperiences = () => api.get("/api/experiences");
+export const getExperience = (id: string) => api.get(`/api/experiences/${id}`);
+export const createBooking = (payload: any) => api.post("/api/bookings", payload);
+export const validatePromo = (code: string) =>
+  api.post("/api/promo/validate", { code });
